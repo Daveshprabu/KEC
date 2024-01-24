5a67a0eba4802ded36ee93452d16ad4c36ce9eb3
Daveshprabu <daveshprabu@gmail.com>
2024-01-24 11:04:21 +0530

    two

---
+++ src/Hello.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const Greeting = ({ isUserLoggedIn }) => (
+  <div>
+    {isUserLoggedIn ? (
+      <h1>Welcome</h1>
+    ) : (
+      <h1>Login again</h1>
+    )}
+  </div>
+);
+
+const Hello = () => {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  return (
+    <div>
+      <Greeting isUserLoggedIn={userLoggedIn} />
+      <button onClick={() => setUserLoggedIn(!userLoggedIn)}>
+        {userLoggedIn ? 'Logout' : 'Login'}
+      </button>
+    </div>
+  );
+};
+
+export default Hello;
+ 
